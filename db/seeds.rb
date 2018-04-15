547bc3a4e2ddf6b1a48f107c04a544f7895ecbd7
Ilana Corson <ilana@turing.io>
2018-04-15 14:47:47 -0600

    add feedback for seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,12 @@ require 'csv'
 class Seed
   def self.start
     CSV.foreach("./data/movies.csv", headers: true, header_converters: :symbol) do |row|
-      Movie.create!(
+      movie = Movie.create!(
         title: row[:title],
         description: row[:description],
         director: Director.find_or_create_by(name: row[:director])
       )
+      puts "Created #{movie.title}."
     end
   end
 end
